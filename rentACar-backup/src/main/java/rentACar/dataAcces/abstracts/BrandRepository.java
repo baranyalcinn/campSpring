1679af44551023682efcc78d485a03eecd463de9
Baran YALÇIN <93401550+baranyalcinn@users.noreply.github.com>
2023-11-01 15:14:02 +0300

    Delete rentACar-backup directory

--- rentACar-backup/src/main/java/rentACar/dataAcces/abstracts/BrandRepository.java
+++
@@ -1,8 +0,0 @@
-package rentACar.dataAcces.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import rentACar.entities.concretes.Brand;
-
-public interface BrandRepository extends JpaRepository<Brand, Integer> {
-
-}
